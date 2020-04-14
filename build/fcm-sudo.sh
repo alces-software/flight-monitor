c1f439c0950fff6eb329039e08879c626d7d7a67
Dan Shaw <dan.shaw@alces-flight.com>
2020-04-14 18:40:05 +0100

    Added group

--- build/fcm-sudo.sh
+++ build/fcm-sudo.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 adduser monitor
+groupadd monitor
 
 cat << EOF > /etc/sudoers
 ## Allows people in group monitor to run all commands
@@ -10,4 +11,3 @@ EOF
 usermod –aG monitor monitor
 
 echo "Please set the password for the monitor user with passwd monitor"
-
