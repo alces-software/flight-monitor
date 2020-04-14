d321876212c1f01f83d96a6b3fa180fcc2258c47
Dan Shaw <dan.shaw@alces-flight.com>
2020-04-14 18:27:02 +0100

    Inital commit of fcm-sudo.sh

---
+++ build/fcm-sudo.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+adduser monitor
+
+cat << EOF > /etc/sudoers
+## Allows people in group monitor to run all commands
+%monitor	ALL=(ALL)	ALL
+EOF
+
+usermod –aG monitor monitor
+
+echo "Please set the password for the monitor user with passwd monitor"
+
