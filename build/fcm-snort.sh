e2ff38eb61efe83e2e801dd354fc6525f5fa786d
Dan Shaw <dan.shaw@alces-flight.com>
2020-04-22 18:19:06 +0100

    Tidy up of files/dirs + brief readme update

--- build/fcm-snort.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#Install Snort from REPO
-yum install -e0 -q https://github.com/alces-software/flight-monitor/blob/master/resources/snort-2.9.16-1.centos7.x86_64.rpm?raw=true
-
-#Sym link needed if `snort -v` not found/errors
-#ln -s /usr/lib64/libdnet.so.1.0.1 /usr/lib64/libdnet.1
-
-
