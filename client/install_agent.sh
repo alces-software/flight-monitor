feec0a79eeb769e568fe2877ffee5aa9444eafb3
Dan Shaw <dan.shaw@alces-flight.com>
2020-04-23 16:35:28 +0100

    Rename agent dir and tidy

---
+++ client/install_agent.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+useradd zabbix
+mkdir -p /opt/zabbix
+cd /opt/zabbix
+mkdir -p /opt/zabbix/run
+chown zabbix /opt/zabbix/run
+mkdir -p /var/log/zabbix
+chown zabbix /var/log/zabbix
+
+wget http://fcgateway/resources/zabbix_agent.tgz -O zabbix_agent.tgz
+tar -zxvf zabbix_agent.tgz
+
+
+cat << 'EOF' > /usr/lib/systemd/system/zabbix-agent.service
+[Unit]
+Description=Zabbix Agent
+After=syslog.target
+After=network.target
+
+[Service]
+Environment="CONFFILE=/opt/zabbix/conf/zabbix_agentd.conf"
+EnvironmentFile=-/etc/sysconfig/zabbix-agent
+Type=forking
+Restart=on-failure
+PIDFile=/opt/zabbix/run/zabbix_agentd.pid
+KillMode=control-group
+ExecStart=/opt/zabbix/sbin/zabbix_agentd -c $CONFFILE
+ExecStop=/bin/kill -SIGTERM $MAINPID
+RestartSec=10s
+User=zabbix
+Group=zabbix
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+wget http://fcgateway/resources/zabbix_agentd.conf -O /opt/zabbix/conf/zabbix_agentd.conf
+
+systemctl daemon-reload
+systemctl enable zabbix-agent
+systemctl start zabbix-agent
