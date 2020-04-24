305b4bbbc6bb727227b9925de695974377ee2556
wtaylor119 <will.taylor@alces-software.com>
2020-04-24 14:02:59 +0100

    pdsh script

--- build/fcm-pdsh.sh
+++ build/fcm-pdsh.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
-# Script to setup pdsh / define genders 
+# Script to setup pdsh / define genders
+
+cat << EOF > 
+
+node[x-x]   compute,cn
+infra[x-x]  infra
+login[x-x]  login
+viz0[x-x]  login
+master[x-x]  master
+mds[x-x]  storage
+oss[x-x]  storage
+nfs[x-x]  storage
+
+EOF 
+
+ 
