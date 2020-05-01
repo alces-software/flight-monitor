9be35f7c310d73e5bdb0ce92cae39842824338e1
Dan Shaw <dan.shaw@alces-flight.com>
2020-05-01 16:25:06 +0100

    Initial fcops user commit

--- build/fcm-fcops-user.sh
+++ build/fcm-fcops-user.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 # Script to setup privileged fcops user
 
+#Add user
+useradd -d /users/fcops -s /bin/bash fcops
+
+#Setup Pub key for fcops
+ssh-keygen -t rsa -b 4096
+
+echo "Add ~fcops/.ssh/id_rsa.pub key to auth keys on ops-hub"
