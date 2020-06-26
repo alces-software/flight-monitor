3216d3257273316e124549d52714a92be4bf10a1
Dan Shaw <dan.shaw@alces-flight.com>
2020-06-26 11:27:26 +0100

    Update hostname when differs from fcgateway

--- build/fcm-webserver.sh
+++ build/fcm-webserver.sh
@@ -26,6 +26,9 @@ wget https://raw.githubusercontent.com/alces-software/flight-monitor/master/clie
 wget https://raw.githubusercontent.com/alces-software/flight-monitor/master/resources/zabbix_agentd.conf -O /opt/zabbix/srv/resources/zabbix_agentd.conf -q
 wget https://www.zabbix.com/downloads/4.4.5/zabbix_agent-4.4.5-linux-3.0-amd64-static.tar.gz -O /opt/zabbix/srv/resources/zabbix_agent.tgz -q
 
+#Hostname change when people give u VMs that aren't called fcgateway...
+sed -i s/fcgateway/$(hostname -s)/g /opt/zabbix/srv/resources/*
+
 echo "Starting Nginx Services"
 systemctl enable nginx
 systemctl start nginx
