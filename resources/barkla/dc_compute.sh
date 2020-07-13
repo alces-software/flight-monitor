453fa6849589fea50b0854e124ed8717b5155312
Dan Shaw <dan.shaw@alces-flight.com>
2020-07-13 14:08:49 +0100

    IB check update

--- resources/barkla/dc_compute.sh
+++ resources/barkla/dc_compute.sh
@@ -3,13 +3,12 @@
 RED='\033[0;31m'
 NC='\033[0m'
 
-echo -e "${RED}check ib link status${NC}" ; pdsh -f1 -g cn 'ibstatus' ; 
+# Prints any nodes where ibstatus doesn't report a link as ACTIVE
+echo -e "${RED}check ib link status${NC}" ; pdsh -f1 -g cn "ibstatus |grep 'state' "|grep -v 'ACTIVE\|LinkUp' ;
 
-#echo -e "${RED}check compute local disk space${NC}" ; pdsh -g cn 'df -h / /tmp /var' ; 
 # Prints filesystem and usage col, removes % and flags if above 80% usage
 echo -e "${RED}check compute local disk space${NC}" ; pdsh -g cn "df -h / /tmp /var |awk '{print \$1,\$5}'" |sed s/%//g |awk '$3>80' |grep -v "Filesystem Use" ;
 
-#echo -e "${RED}check for reboots${NC}" ; pdsh -g cn uptime ; 
 # Converts uptime to hours and flags nodes with less than 72 hour uptime
 echo -e "${RED}check for reboots${NC}" ; pdsh -g cn "cat /proc/uptime |awk '{print int(\$1/3600)}'" |awk '$2<72' ; 
 
