e5d942c9205eb04bbf3481512b3e66dced6cab3e
Dan Shaw <dan.shaw@alces-flight.com>
2020-07-13 12:24:47 +0100

    Tidy resources dir

--- resources/check-scripts/check_user_procs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-pdsh -g cn '/opt/service/check_user_procs.sh' | grep BAD
-
-
--- resources/check-scripts/check_zombie.sh
+++
@@ -1,3 +0,0 @@
-pdsh -g cn "ps axo pid=,stat= | grep Z" 2> /dev/null
-pdsh -g login "ps axo pid=,stat= | grep Z" 2> /dev/null
-pdsh -g viz "ps axo pid=,stat= | grep Z" 2> /dev/null
