7df841ee4ca61a2a105ffd13b8e306fb5aacc237
Dan Shaw <dan.shaw@alces-flight.com>
2020-07-13 10:47:12 +0100

    inital hub script commit

---
+++ hub/hub-notes.md
@@ -0,0 +1,2 @@
+# Directory for scripts to run on hub
+* hub-vpn-client.sh - Add new client to cluster VPN
---
+++ hub/hub-vpn-client.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Script to add new FC gw client to 'fcops' VPN
+# Add new user + set password
+# Push new IP
+# Restart VPN
+
