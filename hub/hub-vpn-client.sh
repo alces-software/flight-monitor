9e10eef9b7b42af23cb8fc551be2af1e7d661907
Dan Shaw <dan.shaw@alces-flight.com>
2020-07-13 11:13:29 +0100

    hub vpn client script

--- hub/hub-vpn-client.sh
+++ hub/hub-vpn-client.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 # Script to add new FC gw client to 'fcops' VPN
-# Add new user + set password
-# Push new IP
-# Restart VPN
+# Root Check
+if [ `whoami` != 'root' ]
+	then 
+		echo "Please run this script as the root user"
+		exit
+fi
 
+# Add new user + set password
+echo -n "Enter new client username: "; read CLIENTNAME
+echo -n "Enter new client password: "; read PASSWORD
+useradd $CLIENTNAME --shell=/sbin/nologin
+echo ${PASSWORD} | passwd --stdin $CLIENTNAME
+
+touch /etc/openvpn/ccd-cluster/$CLIENTNAME
+# Push new IP
+echo -n "Enter IP to push to new client [format - 10.178.0.XX 10.178.0.XY]: "; read IP
+cat << EOF > /etc/openvpn/ccd-cluster/$CLIENTNAME
+ifconfig-push ${IP}
+EOF
+
+# Restart VPN
+systemctl restart openvpn@cluster
