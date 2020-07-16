f1d4e3845f896b1293e7ace652b11ecd0816a379
Dan Shaw <dan.shaw@alces-flight.com>
2020-07-16 16:39:06 +0100

    Updated Controller DC Script

--- resources/barkla/dc_controller.sh
+++ resources/barkla/dc_controller.sh
@@ -7,18 +7,58 @@ d=$(date +%d)
 RED='\033[0;31m'
 NC='\033[0m' 
 
-# Converts uptime to hours and flags nodes with less than 72 hour uptime
-echo -e "${RED}check for reboots${NC}" ; pdsh -g cn "cat /proc/uptime |awk '{print int(\$1/3600)}'" |awk '$2<72' ;
+function check_uptime {
+if cont_uptime=$(cat /proc/uptime |awk '{print int($1/3600)}' |awk '$1<72 { $1 = "Uptime is: " $1 " hours" ; print}'") && [ -z "$cont_uptime" ]
+then
+        echo "Controller Uptime is OK - above 72 hours"
+else
+        echo "Controller Uptime Check Failed"
+        echo -e "$cont_uptime"
+fi
+}
 
-echo -e "${RED}check var log messages:${NC}" ; cat /var/log/messages | grep -v systemd | grep -v journal | grep -v desktop | grep -v gnome | grep -v megaraid | grep -v tftpd | grep -v dhcpd | grep -v krb5 | grep -v rosalind-module | grep -v fm0_sm | grep "$d" | grep -vi named | grep -vi oxford ;
+function check_mem {
+if cont_mem=$(free -m |grep Mem: |awk '{print int($3/$2*100)}' |awk '$1>80 { $1 = "Mem used is: " $1 "%" ; print}'") && [ -z "$cont_mem" ]
+then
+	echo "Memory Usage on controller is ok - Less than 80%"
+else
+	echo "Memory Usage on controller check failed"
+	echo -e "$cont_mem"
+fi
 
-echo -e "${RED}check controller memory:${NC}" ; free -hm ; 
+if cont_swap=$(free -m |grep Swap: |awk '{print int($3/$2*100)}' |awk '$1>80 { $1 = "Mem used is: " $1 "%" ; print}'") && [ -z "$cont_swap" ]
+then
+	echo "Swap Usage on controller is ok - Less than 80%"
+else
+	echo "Swap Usage on controller check failed"
+	echo -e "$cont_swap"
+fi
+}
+
+function check_disk_space {
+if root_space=$(df -h / |awk '{print $5}' |grep -v "Use%" |sed s/%//g |awk '$1>80 { $1 = "Root filesystem usage is: " $1 "%" ; print}'") && [ -z "$root_space" ]
+then
+        echo "Space on / on controller is ok - Less than 80%"
+else
+        echo "/ filesystem check on controller failed"
+        echo -e "$root_space"
+fi
+
+if boot_space=$(df -h /boot |awk '{print $5}' |grep -v "Use%" |sed s/%//g |awk '$1>80 { $1 = "Boot filesystem usage is: " $1 "%" ; print}'") && [ -z "$boot_space" ]
+then
+        echo "Space on /boot on controller is ok - Less than 80%"
+else
+        echo "/boot filesystem check on controller failed"
+        echo -e "$boot_space"
+fi
+}
 
 
-echo -e "{RED}check controller memory:${NC}" ; free -m |grep Mem: |awk '{print int($3/$2*100)}' |awk '$1>80 { $1 = "Mem used is: " $1 "%" ; print}' ; 
+function run_check {
+echo -e "${RED}Running checks${NC}"
+check_uptime
+check_mem
+check_disk_space
+}
 
-echo -e "{RED}check swap used:${NC}" ; free -m |grep Swap: |awk '{print int($3/$2*100)}' |awk '$1>80 { $1 = "Swap used is: " $1 "%" ; print}' ; 
-
-
-#Only outputs if usage is above 80% in / or /boot
-echo -e "${RED}check controller disk space:${NC}" ; df -h / /boot |awk '{print $1,$5}' |sed s/%//g |awk '$3>80' |grep -v "Filesystem Use";     
+run_check
