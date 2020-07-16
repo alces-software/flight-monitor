0c2e9952e1dbcac6ccaac2b501fd05eb45885f87
Dan Shaw <dan.shaw@alces-flight.com>
2020-07-16 13:27:48 +0100

    Updated Infra DC Script

--- resources/barkla/dc_infra.sh
+++ resources/barkla/dc_infra.sh
@@ -5,11 +5,42 @@ d=$(date +%d)
 RED='\033[0;31m'
 NC='\033[0m'
  
-
-# converts uptime to hours and flags nodes with less than 72 hours uptime
-"echo -e '${RED}check for reboots${NC}' ; pdsh -g infra "cat /proc/uptime |awk '{print int(\$1/3600)}'" |awk '$2<72' ;
-
-echo -e '${RED}check headnode memory usage:${NC}' ; pdsh -g infra free -m |grep Mem: |awk '{print int($3/$2*100)}' |awk '$1>80 { $1 = "Mem used is: " $1 "%" ; print}' ;
-
-echo -e '${RED}check headnode swap usage:${NC}' ; pdsh -g infra free -m |grep Swap: |awk '{print int($3/$2*100)}' |awk '$1>80 { $1 = "Swap used is: " $1 "%" ; print}' ;  
  
+function infra_uptime {
+if infra_uptimes=$(pdsh -g infra "cat /proc/uptime |awk '{print int(\$1/3600)}' |awk '\$1<72 { \$1 = \"Uptime is: \" \$1 \" hours\" ; print}'") && [ -z "$infra_uptime" ]
+then
+        echo "Infra Uptimes are OK - above 72 hours"
+else
+        echo "Infra Uptime Check Failed"
+        echo -e "$infra_uptimes"
+fi
+}
+
+function check_mem {
+if infra_mem=$(pdsh -g infra "free -m |grep Mem: |awk '{print int(\$3/\$2*100)}' |awk '\$1>80 { \$1 = \"Mem used is: \" \$1 \"%\" ; print}'") && [ -z "$infra_mem" ]
+then
+	echo "Memory Usage on infra is ok - Less than 80%"
+else
+	echo "Memory Usage on infra check failed"
+	echo -e "$infra_mem"
+fi
+}
+
+function check_swap {
+if infra_swap=$(pdsh -g infra "free -m |grep Swap: |awk '{print int(\$3/\$2*100)}' |awk '\$1>80 { \$1 = \"Swap used is: \" \$1 \"%\" ; print}'") && [ -z "$infra_swap" ]
+then
+        echo "Swap Usage on infra is ok - Less than 80%"
+else
+        echo "Swap Usage on infra check failed"
+        echo -e "$infra_swap"
+fi
+}
+
+function run_check {
+echo -e "${RED}Running checks${NC}"
+infra_uptime
+check_mem
+check_swap
+}
+
+run_check
