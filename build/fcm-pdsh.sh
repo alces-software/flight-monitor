00cc25af3c648e5ac934f621ee47ac1e09f58b65
Dan Shaw <dan.shaw@alces-flight.com>
2020-07-17 11:29:44 +0100

    init pdsh setup

--- build/fcm-pdsh.sh
+++ build/fcm-pdsh.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 # Script to setup pdsh / define genders
 
-cat << EOF > 
+yum -y -e0 install pdsh pdsh-mod-genders -q
+
+touch /etc/genders
+
+cat << EOF > /etc/genders
 
 node[x-x]   compute,cn
 infra[x-x]  infra
 login[x-x]  login
 viz0[x-x]  login
-master[x-x]  master
+master[x-x]  masters
 mds[x-x]  storage
 oss[x-x]  storage
 nfs[x-x]  storage
 
 EOF 
 
- 
