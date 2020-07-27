1edf61ca0f4e2f1f54dbd67ccd82accef0b57968
Dan Shaw <dan.shaw@alces-flight.com>
2020-07-27 17:32:32 +0100

    Add zabbix_sudo script to default fcgateway resources

--- build/fcm-webserver.sh
+++ build/fcm-webserver.sh
@@ -25,6 +25,7 @@ wget https://raw.githubusercontent.com/alces-software/flight-monitor/master/reso
 wget https://raw.githubusercontent.com/alces-software/flight-monitor/master/client/install_agent.sh -O /opt/zabbix/srv/resources/install_agent.sh -q
 wget https://raw.githubusercontent.com/alces-software/flight-monitor/master/resources/zabbix_agentd.conf -O /opt/zabbix/srv/resources/zabbix_agentd.conf -q
 wget https://www.zabbix.com/downloads/4.4.5/zabbix_agent-4.4.5-linux-3.0-amd64-static.tar.gz -O /opt/zabbix/srv/resources/zabbix_agent.tgz -q
+wget https://raw.githubusercontent.com/alces-software/flight-monitor/master/resources/zabbix_sudo.sh -O /opt/zabbix/srv/resources/zabbix_sudo.sh -q
 
 #Hostname change when people give u VMs that aren't called fcgateway...
 sed -i s/fcgateway/$(hostname -s)/g /opt/zabbix/srv/resources/*
