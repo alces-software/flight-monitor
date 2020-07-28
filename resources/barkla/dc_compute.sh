d521ca73f0ec5f3626aa7541f5a6cd7019052f0b
wtaylor119 <will.taylor@alces-software.com>
2020-07-28 12:09:01 +0100

    Fixed new-line dc_compute script

--- resources/barkla/dc_compute.sh
+++ resources/barkla/dc_compute.sh
@@ -5,16 +5,16 @@ NC='\033[0m'
 
 export iblink=$(pdsh -f1 -g cn ibstatus |grep -i state ) ; 
 
-echo -e "${RED}check ib link status${NC}" ; echo '```'$iblink'```' ; 
+echo -e "${RED}check ib link status${NC}" ; echo '```'"$iblink"'```' ; 
 
 export disk=$(pdsh -g cn df -h / /tmp /var | grep -v "Filesystem Use") ; 
 
-echo -e "${RED}check compute local disk space${NC}" ; echo '```'$disk'```' ; 
+echo -e "${RED}check compute local disk space${NC}" ; echo '```'"$disk"'```' ; 
 
 export reboots=$(pdsh -g cn uptime) ; 
 
-echo -e "${RED}check for reboots${NC}" ; echo '```'$reboots'```' ; 
+echo -e "${RED}check for reboots${NC}" ; echo '```'"$reboots"'```' ; 
 
 export ipmi=$(metal ipmi -g cn -k 'sel elist') ; 
 
-echo -e "${RED}check compute ipmi for any messages${NC}" ; echo '```'$ipmi'```' ;  
+echo -e "${RED}check compute ipmi for any messages${NC}" ; echo '```'"$ipmi"'```' ;  
