05c2a6d49f6ef91147355df9defde9de8471577a
Dan Shaw <dan.shaw@alces-flight.com>
2020-08-05 11:36:24 +0100

    udpated user params - agent

--- client/install_agent.sh
+++ client/install_agent.sh
@@ -46,12 +46,32 @@ WantedBy=multi-user.target
 EOF
 
 cat << 'EOF' > /opt/zabbix/custom_checks/user_params.conf
-UserParameter=corosync,bash /opt/nagios/nagios-plugins/check_corosync
-UserParameter=haops,bash /opt/nagios/nagios-plugins/check_ha_ops.sh
-UserParameter=multipath,bash /opt/nagios/nagios-plugins/check_multipath
-UserParameter=psu,bash /opt/nagios/nagios-plugins/check_PSUs
-UserParameter=mdarray,bash /opt/nagios/nagios-plugins/check_dellMDarray
-UserParameter=varlogmessages,bash /opt/nagios/nagios-plugins/check_log
+UserParameter=corosync,bash /opt/zabbix/scripts/check_corosync
+UserParameter=haops,bash /opt/zabbix/scripts/check_ha_ops.sh
+UserParameter=multipath,bash /opt/zabbix/scripts/check_multipath
+UserParameter=psu,cat /opt/zabbix/logs/psu.out
+UserParameter=mdarray,bash /opt/zabbix/scripts/check_dellMDarray
+UserParameter=dirvish,bash /opt/zabbix/scripts/check_dirvish /mnt/backup
+UserParameter=vncrunning,bash /opt/zabbix/scripts/check_vnc_running.sh 36 72
+UserParameter=userquota,bash /opt/zabbix/scripts/check_quota_and_mount.sh /export/users 1
+UserParameter=sysdisk,bash /opt/zabbix/scripts/check_PERC_H7X0 0 0
+UserParameter=backupdisk,bash /opt/zabbix/scripts/check_PERC_H7X0 1 0
+UserParameter=backupmount,findmnt -nr -o source -T /backup -O rw > /dev/null && echo 0 || echo 1
+UserParameter=tmpmount,findmnt -nr -o source -T /tmp -O rw > /dev/null && echo 0 || echo 1
+UserParameter=usermount,findmnt -nr -o source -T /users -O rw > /dev/null && echo 0 || echo 1
+UserParameter=varmount,findmnt -nr -o source -T /var -O rw > /dev/null && echo 0 || echo 1
+UserParameter=bootmount,findmnt -nr -o source -T /boot -O rw > /dev/null && echo 0 || echo 1
+UserParameter=lustremount,findmnt -nr -o source -T /mnt/lustre -O rw > /dev/null && echo 0 || echo 1
+UserParameter=zombieproc,bash /opt/zabbix/scripts/check_zombie.sh
+UserParameter=ost1,bash /opt/zabbix/scripts/check_PERC_H7X0 0 1
+UserParameter=ost2,bash /opt/zabbix/scripts/check_PERC_H7X0 1 1
+UserParameter=ost3,bash /opt/zabbix/scripts/check_PERC_H7X0 2 1
+UserParameter=ost4,bash /opt/zabbix/scripts/check_PERC_H7X0 3 1
+UserParameter=ost5,bash /opt/zabbix/scripts/check_PERC_H7X0 4 1
+UserParameter=ost6,bash /opt/zabbix/scripts/check_PERC_H7X0 5 1
+UserParameter=xserver,bash /opt/zabbix/scripts/check_procs -c 1 --command X
+UserParameter=ecc,bash /opt/zabbix/scripts/check_ECC-IPMI
+UserParameter=temps,bash bash /opt/zabbix/scripts/check_inlettemps.sh
 EOF
 
 
