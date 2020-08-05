4af294e36db15680e46c4b2e166599b16799c6e3
Dan Shaw <dan.shaw@alces-flight.com>
2020-08-05 12:25:33 +0100

    fix sudo issue in agent install

--- client/install_agent.sh
+++ client/install_agent.sh
@@ -15,8 +15,8 @@ tar -zxvf zabbix_agent.tgz
 
 
 #Add zabbix user to sudoers
-touch /etc/sudoers.d/zabbix-monitor
-cat << 'EOF' >> /etc/sudoers.d/zabbix-monitor
+touch /etc/sudoers.d/zabbix
+cat << 'EOF' > /etc/sudoers.d/zabbix
 Cmnd_Alias ZABBIX = /opt/MegaRAID/MegaCli/MegaCli64 -ldinfo *, /usr/sbin/crm_mon -s, /usr/sbin/multipath -ll, /usr/bin/ipmitool sensor, /usr/bin/SMcli -d -v, /usr/bin/ipmitool sel elist, /usr/sbin/nvme, /usr/bin/grep *, /usr/sbin/repquota, /usr/bin/sinfo *, /usr/sbin/lnetctl route show -v
 zabbix    ALL=(ALL)       NOPASSWD: ZABBIX
 EOF
