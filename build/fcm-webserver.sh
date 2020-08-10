851e9903c94530c09e1244013ae09e3d88bd062f
Dan Shaw <dan.shaw@alces-flight.com>
2020-08-10 12:23:01 +0100

    update tar cmd in fcm build

--- build/fcm-webserver.sh
+++ build/fcm-webserver.sh
@@ -30,7 +30,7 @@ wget https://raw.githubusercontent.com/alces-software/flight-monitor/master/reso
 
 echo "Downloading Custom Zabbix Conf"
 wget https://github.com/alces-software/flight-monitor/blob/master/resources/zabbix_custom_checks.tar.gz?raw=true -O /opt/zabbix/srv/resources/zabbix_checks.tar.gz -q
-tar -zxvf /opt/zabbix/srv/resources/zabbix_checks.tar.gz
+tar -zxvf /opt/zabbix/srv/resources/zabbix_checks.tar.gz -C /opt/zabbix/srv/resources/
 
 
 #Hostname change when people give u VMs that aren't called fcgateway...
