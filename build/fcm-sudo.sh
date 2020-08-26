486a1636b85911cf0c2716a568c8e4fd8faf1cb6
Dan Shaw <dan.shaw@alces-flight.com>
2020-08-26 14:35:03 +0100

    removed old sudo setup script

--- build/fcm-sudo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-useradd -G monitor monitor
-
-echo '## Allows people in group monitor to run all commands' | sudo EDITOR='tee -a' visudo
-echo '%monitor	ALL=(ALL)	ALL' | sudo EDITOR='tee -a' visudo
-
-echo "Please set the password for the monitor user with passwd monitor"
