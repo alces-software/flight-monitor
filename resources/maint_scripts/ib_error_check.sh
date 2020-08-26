3ad5f81615a80586559c68189aef4ca0440e46c0
Dan Shaw <dan.shaw@alces-flight.com>
2020-08-26 15:35:53 +0100

    misc maint_scripts -> flight-monitor-resources

--- resources/maint_scripts/ib_error_check.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# a script to check for infiniband errors
-
-ibqueryerrors | grep -vi PortXmitWait | grep -vi PortRcvSwitchRelayErrors | grep -vi for | wc -l 
--- resources/maint_scripts/infiniband_checker.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# The new script to check for infiniband errors
-
-ibqueryerrors | grep -i 'linkdowned\|portrcverrors\|portrcvremotephysicalerrors\|portrcvswitchrelayerrors\|constrainterrors\|rcvremotephyserrors\|symbolerrors\|vl15drop\|xmtdiscards' | wc -l    
