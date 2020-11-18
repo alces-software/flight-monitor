625308fd2ec28135f0a5935d57da3b39c7249e11
Dan Shaw <dan.shaw@alces-flight.com>
2020-11-18 16:03:21 +0000

    barkla node specific install test

---
+++ collectd/install/client/collectd-client_barkla.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+#Install collectd and deps
+yum install -e0 -y epel-release
+yum install -e0 -y collectd
+yum install -e0 -y rrdtool rrdtool-devel rrdtool-perl perl-HTML-Parser perl-JSON collectd-rrdtool
+
+
+#Setup config
+cat << 'EOF' > /etc/collectd.conf
+
+Hostname    "insert_hostname"
+FQDNLookup   true
+BaseDir     "/var/lib/collectd"
+PIDFile     "/var/run/collectd.pid"
+PluginDir   "/usr/lib64/collectd"
+TypesDB     "/usr/share/collectd/types.db"
+
+Interval     10
+
+LoadPlugin logfile
+
+<Plugin logfile>
+	LogLevel info
+	File "/var/log/collectd.log"
+	Timestamp true
+	PrintSeverity false
+</Plugin>
+
+LoadPlugin contextswitch
+LoadPlugin cpu
+LoadPlugin cpufreq
+LoadPlugin df
+LoadPlugin disk
+LoadPlugin interface
+LoadPlugin load
+LoadPlugin memory
+LoadPlugin network
+LoadPlugin nfs
+LoadPlugin rrdtool
+LoadPlugin uptime
+LoadPlugin users
+
+<Plugin network>
+	<Server "10.10.0.2" "25826">
+	</Server>
+</Plugin>
+<Plugin rrdtool>
+	DataDir "/opt/collectd/rrd/"
+	CreateFilesAsync false
+	CacheTimeout 120
+	CacheFlush   900
+	WritesPerSecond 50
+</Plugin>
+
+LoadPlugin exec
+<Plugin exec>
+        Exec "flight" "/usr/local/bin/lustre-stats-wrapper-collectd.sh"
+</Plugin>
+Include "/etc/collectd.d"
+EOF
+
+sed -i s/insert_hostname/$(hostname)/g /etc/collectd.conf
+
+systemctl enable collectd
+systemctl start collectd
