0022a7f3fffb9b13ff74bd0e38ee8cd7b221ee3f
Dan Shaw <dan.shaw@alces-flight.com>
2020-12-17 20:23:17 +0000

    zabbix setup notes

--- hub/adoption/zabbix_setup.sh
+++ hub/adoption/zabbix_setup.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 # Script to setup zabbix on newly adopted node
 # This script should probably be called with whether or not it is a new node to zabbix or whether it is a node which just needs enabling?
+# If node is already in zabbix then should enable - setup if statement and do with functions
+
+
+
+
+
+enable_node{
+
+}
 
