743716431fe25d8757a1ccb2ff56ad6afadb9f05
Dan Shaw <dan.shaw@alces-flight.com>
2020-12-17 20:01:49 +0000

    Remember to run a pdsh -w in fcops setup

--- hub/adoption/fcops_setup.sh
+++ hub/adoption/fcops_setup.sh
@@ -5,12 +5,14 @@
 FCOPS_PASSWORD=$(curl -s http://cfcgateway/resources/maint_scripts/adopt_config |grep fcops_pass |awk '{print $2}')
 PUB_SSH_KEY=$(curl -s http://cfcgateway/resources/maint_scripts/adopt_config |grep ssh_key |awk '{print $2,$3,$4}')
 
-groupadd -g 64646 fcops
-useradd -d /home/fcops -s /bin/bash -u 64646 -g 64646 fcops
-echo "$FCOPS_PASSWORD" | passwd --stdin fcops
-echo 'fcops    ALL=(ALL)       NOPASSWD: ALL' > /etc/sudoers.d/fcops
-su - fcops -c 'mkdir -p /home/fcops/.ssh'
-su - fcops -c 'chmod 700 /home/fcops/.ssh'
-su - fcops -c 'touch /home/fcops/.ssh/authorized_keys'
-su - fcops -c 'chmod 640 /home/fcops/.ssh/authorized_keys'
-echo "$PUB_SSH_KEY"  >> /home/fcops/.ssh/authorized_keys
+pdsh -w $1 "
+groupadd -g 64646 fcops ; 
+useradd -d /home/fcops -s /bin/bash -u 64646 -g 64646 fcops ;
+echo "$FCOPS_PASSWORD" | passwd --stdin fcops ;
+echo 'fcops    ALL=(ALL)       NOPASSWD: ALL' > /etc/sudoers.d/fcops ; 
+su - fcops -c 'mkdir -p /home/fcops/.ssh' ;
+su - fcops -c 'chmod 700 /home/fcops/.ssh' ;
+su - fcops -c 'touch /home/fcops/.ssh/authorized_keys' ;
+su - fcops -c 'chmod 640 /home/fcops/.ssh/authorized_keys' ;
+echo "$PUB_SSH_KEY"  >> /home/fcops/.ssh/authorized_keys 
+"
