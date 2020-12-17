ab30625aee2543b2c4516919e093e3096d6c729d
Dan Shaw <dan.shaw@alces-flight.com>
2020-12-17 14:09:13 +0000

    Adoption Skeleton

---
+++ hub/adoption/adopt_node.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# A wrapper script for adopting new nodes [CLOUD]
+
+# Should run fcops setup first
+
+# Then Zabbix install / setup / config
+
+# Then ensure checks will run on this new node
+
+# Possibly also have a slack alert once this has run sucessfully ?
