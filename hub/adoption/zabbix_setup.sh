93aab5e5ee9ff8000929519a816622af0db2d63e
Dan Shaw <dan.shaw@alces-flight.com>
2020-12-17 15:42:04 +0000

    Adoption - zabbix_setup.sh

---
+++ hub/adoption/zabbix_setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Script to setup zabbix on newly adopted node
+# This script should probably be called with whether or not it is a new node to zabbix or whether it is a node which just needs enabling?
+
