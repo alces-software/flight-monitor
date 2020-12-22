3e6bb0126142f81dfcf69db4585b1fac34e1ba0a
Dan Shaw <dan.shaw@alces-flight.com>
2020-12-22 09:39:32 +0000

    Configure Snort

--- snort/install_notes.sh
+++ snort/install_notes.sh
@@ -23,9 +23,33 @@ cd snort-2.9.17
 ./configure --enable-sourcefire && make && sudo make install
 
 #Configre Snort
-
-
+sudo ldconfig
+sudo ln -s /usr/local/bin/snort /usr/sbin/snort
 
 #Setup SNORT USER / DIRS
+sudo groupadd snort
+sudo useradd snort -r -s /sbin/nologin -c SNORT_IDS -g snort
+sudo mkdir -p /etc/snort/rules
+sudo mkdir /var/log/snort
+sudo mkdir /usr/local/lib/snort_dynamicrules
 
-#Grab latest rules
+sudo touch /etc/snort/rules/white_list.rules
+sudo touch /etc/snort/rules/black_list.rules
+sudo touch /etc/snort/rules/local.rules
+
+sudo cp ~/snort_src/snort-2.9.17/etc/*.conf* /etc/snort
+sudo cp ~/snort_src/snort-2.9.17/etc/*.map /etc/snort
+
+#Grab latest rules - registered user
+#OINKCODE=$1
+#wget https://www.snort.org/rules/snortrules-snapshot-29120.tar.gz?oinkcode=$OINKCODE -O ~/registered.tar.gz
+
+#Change of plan - using 2.9 community rules
+cd
+wget https://www.snort.org/downloads/community/community-rules.tar.gz
+tar xvzf community-rules.tar.gz
+sudo cp ~/community-rules/* /etc/snort/rules
+#By default, Snort on CentOS expects to find a number of different rule files which are not included in the community rules. Comment out the unnecessary lines using the next command.
+sudo sed -i 's/include \$RULE\_PATH/#include \$RULE\_PATH/' /etc/snort/snort.conf
+
+
