4037b4ac8c7c07e0319816c3f438cd911903aba4
Dan Shaw <dan.shaw@alces-flight.com>
2020-12-22 09:31:01 +0000

    Download, Install Snort

--- snort/install_notes.sh
+++ snort/install_notes.sh
@@ -5,41 +5,27 @@ sudo yum install -y gcc flex bison zlib libpcap pcre libdnet tcpdump
 sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 sudo yum install -y libnghttp2
 
-#Snort install
-cd /tmp
-wget https://www.snort.org/downloads/archive/snort/daq-2.0.6-1.f21.x86_64.rpm 
-yum localinstall daq-2.0.6-1.f21.x86_64.rpm
-wget https://www.snort.org/downloads/archive/snort/snort-2.9.9.0-1.centos7.x86_64.rpm
-yum localinstall snort-2.9.9.0-1.centos7.x86_64.rpm
-rm -rf daq-2.0.6-1.f21.x86_64.rpm snort-2.9.9.0-1.centos7.x86_64.rpm
-cd
+#Snort install from source
+sudo yum install -y zlib-devel libpcap-devel pcre-devel libdnet-devel openssl-devel libnghttp2-devel luajit-devel
+mkdir ~/snort_src && cd ~/snort_src
+
+#DAQ
+wget https://www.snort.org/downloads/snort/daq-2.0.7.tar.gz
+tar -xvzf daq-2.0.7.tar.gz
+cd daq-2.0.7
+./configure && make && sudo make install
+
+#Snort
+cd ~/snort_src
+wget https://www.snort.org/downloads/snort/snort-2.9.17.tar.gz
+tar -xvzf snort-2.9.17.tar.gz
+cd snort-2.9.17
+./configure --enable-sourcefire && make && sudo make install
+
+#Configre Snort
+
+
 
 #Setup SNORT USER / DIRS
-sudo groupadd snort
-sudo useradd snort -r -s /sbin/nologin -c SNORT_IDS -g snort
-
-sudo mkdir -p /etc/snort/rules
-sudo mkdir /var/log/snort
-sudo mkdir /usr/local/lib/snort_dynamicrules
-
-sudo chmod -R 5775 /etc/snort
-sudo chmod -R 5775 /var/log/snort
-sudo chmod -R 5775 /usr/local/lib/snort_dynamicrules
-sudo chown -R snort:snort /etc/snort
-sudo chown -R snort:snort /var/log/snort
-sudo chown -R snort:snort /usr/local/lib/snort_dynamicrules
 
 #Grab latest rules
-wget https://www.snort.org/rules/snortrules-snapshot-3031.tar.gz?oinkcode=f5dc50fba9177525894ded43ea05cd500d068d2f -O snortrules-snapshot-3031.tar.gz
-wget https://www.snort.org/rules/snortrules-snapshot-3034.tar.gz?oinkcode=f5dc50fba9177525894ded43ea05cd500d068d2f -O snortrules-snapshot-3034.tar.gz
-wget https://www.snort.org/rules/snortrules-snapshot-2983.tar.gz?oinkcode=f5dc50fba9177525894ded43ea05cd500d068d2f -O snortrules-snapshot-2983.tar.gz
-wget https://www.snort.org/rules/snortrules-snapshot-3000.tar.gz?oinkcode=f5dc50fba9177525894ded43ea05cd500d068d2f -O snortrules-snapshot-3000.tar.gz
-wget https://www.snort.org/rules/snortrules-snapshot-29111.tar.gz?oinkcode=f5dc50fba9177525894ded43ea05cd500d068d2f -O snortrules-snapshot-29111.tar.gz
-wget https://www.snort.org/rules/snortrules-snapshot-29130.tar.gz?oinkcode=f5dc50fba9177525894ded43ea05cd500d068d2f -O snortrules-snapshot-29130.tar.gz
-wget https://www.snort.org/rules/snortrules-snapshot-29141.tar.gz?oinkcode=f5dc50fba9177525894ded43ea05cd500d068d2f -O snortrules-snapshot-29141.tar.gz
-wget https://www.snort.org/rules/snortrules-snapshot-29150.tar.gz?oinkcode=f5dc50fba9177525894ded43ea05cd500d068d2f -O snortrules-snapshot-29150.tar.gz
-wget https://www.snort.org/rules/snortrules-snapshot-29151.tar.gz?oinkcode=f5dc50fba9177525894ded43ea05cd500d068d2f -O snortrules-snapshot-29151.tar.gz
-wget https://www.snort.org/rules/snortrules-snapshot-29160.tar.gz?oinkcode=f5dc50fba9177525894ded43ea05cd500d068d2f -O snortrules-snapshot-29160.tar.gz
-wget https://www.snort.org/rules/snortrules-snapshot-29161.tar.gz?oinkcode=f5dc50fba9177525894ded43ea05cd500d068d2f -O snortrules-snapshot-29161.tar.gz
-wget https://www.snort.org/rules/snortrules-snapshot-29170.tar.gz?oinkcode=f5dc50fba9177525894ded43ea05cd500d068d2f -O snortrules-snapshot-29170.tar.gz
-
