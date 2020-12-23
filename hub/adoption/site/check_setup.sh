0f2f4d3faaef5cab436b1fa4a67bff02493067da
Dan Shaw <dan.shaw@alces-flight.com>
2020-12-23 11:39:24 +0000

    curl slack token where needed

--- hub/adoption/site/check_setup.sh
+++ hub/adoption/site/check_setup.sh
@@ -2,13 +2,13 @@
 #Script to ensure node is setup sucessfully + in genders file on cfcgateway
 #Alert to slack either way?
 
-
+SLACK=$(grep slack_token /opt/zabbix/srv/resources/maint_scripts/adopt_config |awk '{print $2}')
 
 msg="
 "$NEW_NODE" has been adopted into the ops team processes sucessfully!
 "
 
-SLACK_TOKEN="<REDACTED>"
+SLACK_TOKEN="$SLACK"
 cat <<EOF | curl --silent --output /dev/null --data @- -X POST -H "Authorization: Bearer $SLACK_TOKEN" -H 'Content-Type: application/json' https://slack.com/api/chat.postMessage
 {
   "text": "$msg",
