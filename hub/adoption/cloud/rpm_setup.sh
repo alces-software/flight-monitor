cd0f7957df6cf9d108236ff8667c47beceacf323
Dan Shaw <dan.shaw@alces-flight.com>
2021-01-05 10:53:37 +0000

    cloud rpm setup

--- hub/adoption/cloud/rpm_setup.sh
+++ hub/adoption/cloud/rpm_setup.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 # Script to check RPMs which have previously been installed to the new nodes gender group + installs packages where necessary
+
+#Check if any RPMs for compute nodes exist on this cluster
+
+
