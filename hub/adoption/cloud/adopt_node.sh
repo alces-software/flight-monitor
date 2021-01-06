52c594bfaa82444178165e48e378c200a6e5e7e0
Dan Shaw <dan.shaw@alces-flight.com>
2021-01-06 15:03:07 +0000

    No idea why pre-xmas Dan was doing that horrible SSH

--- hub/adoption/cloud/adopt_node.sh
+++ hub/adoption/cloud/adopt_node.sh
@@ -50,10 +50,7 @@ NEW_NODE_SHORT=$(echo $NEW_NODE |cut -d"." -f1)
 # Should run fcops setup first
 
 # Runs from chead1 of cloud cluster - keys from fcops@cfcgateway -> root@chead1 should exist
-ssh -tt root@chead1 <<-'EOF'
-curl http://cfcgateway/resources/adoption/fcops_setup.sh "$NEW_NODE" |/bin/bash
-exit
-EOF
+ssh root@chead1 'curl http://cfcgateway/resources/adoption/fcops_setup.sh "$NEW_NODE" |/bin/bash'
 
 # Then Zabbix install / setup / config
 
