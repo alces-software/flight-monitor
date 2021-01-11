2ed4ba07fadc22e1a56416b903d3779165bbb5bb
Dan Shaw <dan.shaw@alces-flight.com>
2021-01-11 16:50:07 +0000

    fix typo

--- hub/new_fcops_vpn_cluster.sh
+++ hub/new_fcops_vpn_cluster.sh
@@ -14,7 +14,7 @@ touch /etc/openvpn/ccd-cluster/$CLUSTERNAME
 
 LAST_IP=$(tail /etc/hosts -n 1 |awk '{print $1}' |cut -d . -f4)
 NEW_IP=$(expr $LAST_IP + 4)
-NEW_IP_2=$(expr $LAST_IP = 5)
+NEW_IP_2=$(expr $LAST_IP + 5)
 
 echo "ifconfig-push 10.178.0."$NEW_IP" 10.178.0."$NEW_IP_2"" > /etc/openvpn/ccd-cluster/$CLUSTERNAME
 
