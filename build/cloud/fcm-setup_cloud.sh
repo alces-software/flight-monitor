72d7da1627fce892334dc15fbf42d5be29c7637b
Dan Shaw <dan.shaw@alces-flight.com>
2021-01-12 17:02:39 +0000

    pull cloud versions of build scripts

--- build/cloud/fcm-setup_cloud.sh
+++ build/cloud/fcm-setup_cloud.sh
@@ -48,8 +48,8 @@ echo "Downloading scripts from flight-monitor github"
 wget https://raw.githubusercontent.com/alces-software/flight-monitor/master/resources/zabbix_proxy.conf -O /etc/zabbix/zabbix_proxy.conf --no-check-certificate
 wget https://raw.githubusercontent.com/alces-software/flight-monitor/master/resources/zabbix_agentd.monitor.conf -O /etc/zabbix/zabbix_agentd.conf --no-check-certificate
 wget https://raw.githubusercontent.com/alces-software/flight-monitor/master/build/vpn-client.sh -O /tmp/fcm-vpnclient.sh --no-check-certificate
-wget https://raw.githubusercontent.com/alces-software/flight-monitor/master/build/fcm-webserver.sh -O /tmp/fcm-webserver.sh --no-check-certificate
-wget https://raw.githubusercontent.com/alces-software/flight-monitor/master/build/fcm-fcops-user.sh -O /tmp/fcm-fcops-user.sh --no-check-certificate
+wget https://raw.githubusercontent.com/alces-software/flight-monitor/master/build/cloud/fcm-webserver.sh -O /tmp/fcm-webserver.sh --no-check-certificate
+wget https://raw.githubusercontent.com/alces-software/flight-monitor/master/build/cloud/fcm-fcops-user.sh -O /tmp/fcm-fcops-user.sh --no-check-certificate
 
 chmod +x /tmp/fcm-vpnclient.sh
 chmod +x /tmp/fcm-webserver.sh
