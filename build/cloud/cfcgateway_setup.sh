1387c91a10831be6128bddbb9dad30eb74c40033
Dan Shaw <dan.shaw@alces-flight.com>
2021-01-12 17:23:56 +0000

    wrapper script

--- build/cloud/cfcgateway_setup.sh
+++ build/cloud/cfcgateway_setup.sh
@@ -2,15 +2,17 @@
 # A wrapper script to install necessary things + create a cfcgateway
 # This wrapper script assumes that you have setup necessary VPN stuff
 
-
-
 #Curl + Run original cloud setup script
 curl https://raw.githubusercontent.com/alces-software/flight-monitor/master/build/cloud/fcm-setup_cloud.sh | /bin/bash
 
 #This initial setup should then pull other scripts to /tmp so we don't have to keep curling files
 
+#Adds gw to ops hub cluster VPN - will prompt for user and pass atm
+bash /tmp/fcm-vpnclient.sh
 
+#Sets up fcops user - this should be run bofore webserver bc permissions
+bash /tmp/fcm-fcops-user.sh
 
-bash /tmp/fcm-vpnclient.sh #Adds gw to ops hub cluster VPN
-bash /tmp/fcm-webserver.sh #Sets up Nginx + Zabbix Proxy/Agent
-bash /tmp/fcm-fcops-user.sh #Sets up fcops user
+#Sets up Nginx + Zabbix Proxy/Agent
+bash /tmp/fcm-webserver.sh
+
