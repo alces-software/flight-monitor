dd055cc6ca934875192537c94633da7d1a376f04
Dan Shaw <dan.shaw@alces-flight.com>
2021-02-14 10:06:10 +0000

    rpm setup init setup

--- hub/adoption/site/rpm_setup.sh
+++ hub/adoption/site/rpm_setup.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 # Script to check RPMs which have previously been installed to the new nodes gender group + installs packages where necessary
+#Notes from node072 install
+cd /users/fcops/git/flight-monitor-resources/kelvin2/apps_scripts/compute
+for script in $(ls) ; do pdsh -w node072 "curl http://fcgateway/resources/apps_scripts/$script |/bin/bash" ; done
