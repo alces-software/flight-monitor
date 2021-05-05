d4fea6edc258768d4bdbe696e44c1697da8940e7
Dan Shaw <dan.shaw@alces-flight.com>
2021-05-05 09:24:53 +0100

    fcgw build install gnome desktop

--- build/fcm-setup_cloud.sh
+++ build/fcm-setup_cloud.sh
@@ -19,7 +19,7 @@ systemctl restart firewalld
 # Packages
 echo "Installing Prerequisites"
 yum install -y -e0 vim git epel-release wget -q
-yum groupinstall iy -e0 "GNOME Desktop" -q 
+yum groupinstall -y -e0 "GNOME Desktop" -q 
 yum install -y -e0 s3cmd awscli ipmitool -q 
 yum install -y -e0 httpd yum-plugin-priorities yum-utils createrepo -q
 yum install -y -e0 net-snmp net-snmp-utils -q #Install snmp utils for snmp disco
