2d2325784466b74d283e7ea191c5a0f4d3c53524
Dan Shaw <dan.shaw@alces-flight.com>
2021-08-09 15:19:57 +0100

    Moving things around a little.

--- resources/Tools/memtester_tool/memtester/memtest_tool.sh
+++ resources/Tools/memtester_tool/memtester/memtest_tool.sh
@@ -1,44 +1,6 @@
 #!/bin/bash
 
-source /tmp/slack.txt
-
-
-HOSTNODE=$(hostname | cut -d . -f1) 
-
-HOSTCLUSTER=$(hostname | cut -d . -f3)
-
-msg="
-Memtester running on $HOSTNODE of $HOSTCLUSTER.
-"
-
-
-#Send Message
-
-cat << EOF | curl --data @- -X POST -H "Authorization: Bearer $SLACK_TOKEN" -H 'Content-Type: application/json' https://slack.com/api/chat.postMessage
-{
-  "text": "$msg",
-  "channel": "$HOSTCLUSTER",
-  "as_user": true
-}
-EOF
-
-
 bash ~/memtester/bequiet.sh sudo bash ~/memtester/run_memtester.sh & sleep $1h
 
 sudo killall -9 memtester
- 
-msg="
-Memtester finished running on $HOSTNODE of $HOSTCLUSTER.
-"
-#Send Message
-
-cat <<EOF | curl --data @- -X POST -H "Authorization: Bearer $SLACK_TOKEN" -H 'Content-Type: application/json' https://slack.com/api/chat.postMessage
-{
-  "text": "$msg",
-  "channel": "$HOSTCLUSTER",
-  "as_user": true
-}
-EOF
-
-
 
--- resources/Tools/memtester_tool/run_memtool.sh
+++ resources/Tools/memtester_tool/run_memtool.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+source "/tmp/slack.txt"
+
+HOSTCLUSTER=$(hostname | cut -d . -f3)
+
+msg="
+Memtester running on $1 of $HOSTCLUSTER.
+"
+
 sudo rm -r memtester.tar.gz
 
 sudo tar -zcvf memtester.tar.gz memtester
@@ -8,6 +16,30 @@ pdsh -w $1 "curl 'http://fcgateway/resources/memtester_tool/memtester.tar.gz' > 
 
 pdsh -w $1 "tar -xzvf memtester.tar.gz"
 
+Send Message
+
+cat << EOF | curl --data @- -X POST -H "Authorization: Bearer $SLACK_TOKEN" -H 'Content-Type: application/json' https://slack.com/api/chat.postMessage
+{
+  "text": "$msg",
+  "channel": "$HOSTCLUSTER",
+  "as_user": true
+}
+EOF
+
 pdsh -w	$1 ./memtester/memtest_tool.sh $2
 
 #pdsh -w $1 "rm -r memtester memtester.tar.gz"
+
+msg="
+Memtester finished running on $1 of $HOSTCLUSTER.
+"
+#Send Message
+
+cat <<EOF | curl --data @- -X POST -H "Authorization: Bearer $SLACK_TOKEN" -H 'Content-Type: application/json' https://slack.com/api/chat.postMessage
+{
+  "text": "$msg",
+  "channel": "$HOSTCLUSTER",
+  "as_user": true
+}
+EOF
+
