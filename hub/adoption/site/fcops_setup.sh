8567e6cfc081e706aea7dba4b9e9cb3b725bc808
wtaylor119 <63309978+wtaylor119@users.noreply.github.com>
2021-08-17 12:21:51 +0100

    Update fcops_setup.sh

--- hub/adoption/site/fcops_setup.sh
+++ hub/adoption/site/fcops_setup.sh
@@ -2,10 +2,10 @@
 #Script to setup the fcops user on a cluster
 #This script will be launched from fcgateway but run on controller
 #Requires SSH KEY + FCOPS PASSWORD variables
-FCOPS_PASSWORD=$(cat adopt_config |grep fcops_pass |awk '{print $2}')
-PUB_SSH_KEY=$(cat adopt_config |grep ssh_key |awk '{print $2,$3,$4}')
+FCOPS_PASSWORD=$(curl http://fcgateway/resources/maint_scripts/adopt_config |grep fcops_pass |awk '{print $2}')
+PUB_SSH_KEY=$(curl http://fcgateway/resources/maint_scripts/adopt_config |grep ssh_key |awk '{print $2,$3,$4}')
 
-
+pdsh -w $1 "
 groupadd -g 64646 fcops ; 
 useradd -d /home/fcops -s /bin/bash -u 64646 -g 64646 fcops ;
 echo "$FCOPS_PASSWORD" | passwd --stdin fcops ;
@@ -15,4 +15,4 @@ su - fcops -c 'chmod 700 /home/fcops/.ssh' ;
 su - fcops -c 'touch /home/fcops/.ssh/authorized_keys' ;
 su - fcops -c 'chmod 640 /home/fcops/.ssh/authorized_keys' ;
 echo "$PUB_SSH_KEY"  >> /home/fcops/.ssh/authorized_keys 
-
+"
