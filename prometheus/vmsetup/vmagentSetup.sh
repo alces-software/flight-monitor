acdbd44c960cc449550c4de7efb496843d383156
twshaw646 <112694361+twshaw646@users.noreply.github.com>
2022-11-03 14:42:20 +0000

    Update vmagentSetup.sh

--- prometheus/vmsetup/vmagentSetup.sh
+++ prometheus/vmsetup/vmagentSetup.sh
@@ -1,10 +1,9 @@
+#!/bin/bash
 
 #Add flight-graphs to hosts
 cat << EOF >> /etc/hosts
-
 10.178.0.169  flight-graphs.fcops.alces-flight.com flight-graphs
 EOF
-
 
 #Install vmagent
 mkdir /tmp/vmetrics
@@ -55,38 +54,15 @@ cat << 'EOF' > /opt/vmagent/configs/node-exporter.yml
         replacement: '${1}'
 EOF
 
-cat << 'EOF' > /opt/vmagent/configs/slurm-exporter.yml
-# slurm-exporter
-  - job_name: 'slurm'
-    scrape_interval: 1m # lowered slurm scrape to reduce load on slurmdb
-    file_sd_configs:
-      - files:
-        - '/opt/vmagent/targets/slurm-exporter.yml'
-    # Remove port from hostname label
-    relabel_configs:
-      - source_labels: [__address__]
-        target_label: instance
-        regex: '([^:]+)(:[0-9]+)?'
-        replacement: '${1}'
-EOF
-
 #Add targets to relevant target files
 cat << EOF > /opt/vmagent/targets/node-exporter.yml
 ---
 - targets:
   - fcgateway:9100
-  - node001:9100
-  - node002:9100
-EOF
-
-cat << EOF > /opt/vmagent/targets/slurm-exporter.yml
----
-- targets:
-  - infra02:9101
 EOF
 
 #Create and start service
-export CLUSTER="hyperion"
+export CLUSTER=$(hostname -f | cut -d. -f3)
 
 cat << EOF > /usr/lib/systemd/system/vmagent.service
 [Unit]
@@ -96,7 +72,7 @@ After=network.target
 [Service]
 Type=simple
 WorkingDirectory=/opt/vmagent
-ExecStart=/opt/vmagent/bin/vmagent --promscrape.config=/opt/vmagent/prometheus.yml --remoteWrite.url=http://flight-graphs:8428/api/v1/write --remoteWrite.label=cluster=$CLUSTER --loggerLevel=ERROR --remoteWrite.maxDiskUsagePerURL=5GB
+ExecStart=/opt/vmagent/bin/vmagent --promscrape.config=/opt/vmagent/prometheus.yml --remoteWrite.url=http://flight-graphs:8429/insert/0/prometheus/api/v1/write --remoteWrite.label=cluster=$CLUSTER --loggerLevel=ERROR --remoteWrite.maxDiskUsagePerURL=5GB
 KillMode=control-group
 
 [Install]
