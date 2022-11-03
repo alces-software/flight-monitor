a59c9349f3338af616d4e7eaad56063820fc84ef
twshaw646 <112694361+twshaw646@users.noreply.github.com>
2022-11-03 15:51:26 +0000

    Update lustreExporterSetup.sh

--- prometheus/vmsetup/lustreExporterSetup.sh
+++ prometheus/vmsetup/lustreExporterSetup.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
-mkdir /opt/slurm-exporter
-cd /opt/slurm-exporter
-wget http://fcgateway/resources/metrics/slurm-exporter
-chmod +x /opt/slurm-exporter/slurm-exporter
+mkdir /opt/lustre-exporter
+cd /opt/lustre-exporter
+wget http://fcgateway/resources/metrics/lustre_exporter
+chmod +x /opt/lustre-exporter/lustre_exporter
 
-cat << EOF > /usr/lib/systemd/system/slurm-exporter.service
+cat << EOF > /usr/lib/systemd/system/lustre-exporter.service
 [Unit]
-Description=Prometheus SLURM Exporter
+Description=Lustre exporter service
+After=network.target
 
 [Service]
-ExecStart=/opt/slurm-exporter/slurm-exporter -p 9101 -jN
-Restart=always
-RestartSec=15
+Type=simple
+WorkingDirectory=/opt/lustre-exporter
+ExecStart=/opt/lustre-exporter/lustre_exporter --web.listen-address=:9169 --log.level=error --collector.health=extended --collector.generic=extended --collector.ost=disabled --collector.mdt=extended --collector.mgs=disabled --collector.mds=disabled --collector.client=disabled --collector.lnet=disabled
 
 [Install]
 WantedBy=multi-user.target
 EOF
 
 systemctl daemon-reload
-systemctl enable --now slurm-exporter
+systemctl enable --now lustre-exporter
