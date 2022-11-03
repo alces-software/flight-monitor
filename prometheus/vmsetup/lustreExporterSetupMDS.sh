3eccd0754b576a91a1cbef2083071c7ef89ef1eb
twshaw646 <112694361+twshaw646@users.noreply.github.com>
2022-11-03 14:40:29 +0000

    Update lustreExporterSetupMDS.sh

--- prometheus/vmsetup/lustreExporterSetupMDS.sh
+++ prometheus/vmsetup/lustreExporterSetupMDS.sh
@@ -1,6 +1,9 @@
-mkdir /opt/lustre-exporter
-mv lustre_exporter /opt/lustre-exporter/lustre-exporter
+#!/bin/bash
 
+mkdir /opt/lustre-exporter
+cd /opt/lustre-exporter
+wget http://fcgateway/resources/metrics/lustre_exporter
+chmod +x /opt/lustre-exporter/lustre_exporter
 
 cat << EOF > /usr/lib/systemd/system/lustre-exporter.service
 [Unit]
@@ -10,15 +13,11 @@ After=network.target
 [Service]
 Type=simple
 WorkingDirectory=/opt/lustre-exporter
-ExecStart=/opt/lustre-exporter/lustre_exporter --web.listen-address=:9169 --log.level=error --collector.health=extended --collector.generic=extended --collector.ost=disabled --collector.mdt=extended --collector.mgs=disabled --collector.mds=disabled --collector.client=disabled --collector.lnet=disabled 
-
-[Install]
-WantedBy=multi-user.target
-EOF
+ExecStart=/opt/lustre-exporter/lustre_exporter --web.listen-address=:9169 --log.level=error --collector.health=extended --collector.generic=extended --collector.ost=disabled --collector.mdt=extended --collector.mgs=disabled --collector.mds=disabled --collector.client=disabled --collector.lnet=disabled
 
 [Install]
 WantedBy=multi-user.target
 EOF
 
 systemctl daemon-reload
-systemctl enable --now slurm-exporter
+systemctl enable --now lustre-exporter
