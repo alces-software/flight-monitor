ce487e771bea9ad0ea92ee35343ffa85a098506b
Gary Sangwell <gary.sangwell@alces-software.com>
2022-11-04 16:51:54 +0000

    Add metric exporter discovery install.

---
+++ prometheus/vmsetup/exporterDiscoverySetup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+network="10.10.0.0/16"
+
+# Check vmagent is installed
+if [ ! -d "/opt/vmagent2" ] ; then
+	echo "Error: Install vmagent first."
+	exit 1
+fi
+
+cd /opt/vmagent/bin
+wget --no-check-certificate http://hub/resources/metrics/prometheus-net-discovery -O /opt/vmagent/bin/prometheus-net-discovery
+chmod +x /opt/vmagent/bin/prometheus-net-discovery
+
+cat << EOF > /usr/lib/systemd/system/exporter-discovery.service
+[Unit]
+Description=Prometheus Export Discovery Service
+After=network.target
+
+[Service]
+Type=simple
+WorkingDirectory=/opt/vmagent
+ExecStart=/opt/vmagent/bin/prometheus-net-discovery --networks $network --filesdpath /opt/vmagent/targets/ --interval 24h
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl daemon-reload
+systemctl enable --now exporter-discovery
--- prometheus/vmsetup/fcgateway-setup.sh
+++ prometheus/vmsetup/fcgateway-setup.sh
@@ -4,21 +4,23 @@ mkdir -p /opt/zabbix/srv/resources/metrics
 cd /opt/zabbix/srv/resources/metrics
 
 # Grab binaries from hub
-wget --no-verbose https://hub.fcops.alces-flight.com/resources/metrics/slurm-exporter --no-check-certificate
-wget --no-verbose https://hub.fcops.alces-flight.com/resources/metrics/lustre_exporter --no-check-certificate
-wget --no-verbose https://hub.fcops.alces-flight.com/resources/metrics/gpfs_exporter --no-check-certificate
-wget --no-verbose https://hub.fcops.alces-flight.com/resources/metrics/nvidia_gpu_prometheus_exporter --no-check-certificate
-wget --no-verbose https://hub.fcops.alces-flight.com/resources/metrics/snmp_exporter --no-check-certificate
+wget --no-check-certificate "http://hub/resources/metrics/slurm-exporter"
+wget --no-check-certificate "http://hub/resources/metrics/lustre_exporter"
+wget --no-check-certificate "http://hub/resources/metrics/gpfs_exporter"
+wget --no-check-certificate "http://hub/resources/metrics/nvidia_gpu_prometheus_exporter"
+wget --no-check-certificate "http://hub/resources/metrics/snmp_exporter"
 
 # Grab exporter install scripts from git
 wget --no-verbose https://raw.githubusercontent.com/alces-software/flight-monitor/master/prometheus/vmsetup/vmagentSetup.sh
 wget --no-verbose https://raw.githubusercontent.com/alces-software/flight-monitor/master/prometheus/vmsetup/snmpExporterSetup.sh
+wget --no-verbose https://raw.githubusercontent.com/alces-software/flight-monitor/master/prometheus/vmsetup/exporterDiscoverySetup.sh
 wget --no-verbose https://raw.githubusercontent.com/alces-software/flight-monitor/master/prometheus/vmsetup/nodeExporterSetup.sh
 wget --no-verbose https://raw.githubusercontent.com/alces-software/flight-monitor/master/prometheus/vmsetup/slurmExporterSetup.sh
 wget --no-verbose https://raw.githubusercontent.com/alces-software/flight-monitor/master/prometheus/vmsetup/gpfsExporterSetup.sh
 wget --no-verbose https://raw.githubusercontent.com/alces-software/flight-monitor/master/prometheus/vmsetup/ibExporterSetup.sh
 wget --no-verbose https://raw.githubusercontent.com/alces-software/flight-monitor/master/prometheus/vmsetup/lustreExporterSetup.sh
 wget --no-verbose https://raw.githubusercontent.com/alces-software/flight-monitor/master/prometheus/vmsetup/lustreExporterSetupMDS.sh
+wget --no-verbose https://raw.githubusercontent.com/alces-software/flight-monitor/master/prometheus/vmsetup/nvidiaExporterSetup.sh
 
 chown -R fcops:fcops /opt/zabbix/srv/resources/metrics
 
@@ -28,3 +30,6 @@ bash /opt/zabbix/srv/resources/metrics/vmagentSetup.sh
 # Install node and snmp exporter
 bash /opt/zabbix/srv/resources/metrics/nodeExporterSetup.sh
 bash /opt/zabbix/srv/resources/metrics/snmpExporterSetup.sh
+
+# Install exporter discovery
+bash /opt/zabbix/srv/resources/metrics/exporterDiscoverySetup.sh
