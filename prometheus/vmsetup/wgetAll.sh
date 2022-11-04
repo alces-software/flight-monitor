828f0935b93c64b84182dbb61eb425b3bfe2e32e
twshaw646 <112694361+twshaw646@users.noreply.github.com>
2022-11-04 12:09:09 +0000

    Delete wgetAll.sh

--- prometheus/vmsetup/wgetAll.sh
+++
@@ -1,5 +0,0 @@
-#Grab bins from hub
-wget --no-check-certificate "http://hub/resources/metrics/lustre_exporter"
-wget --no-check-certificate "http://hub/resources/metrics/slurm_exporter"
-wget --no-check-certificate "http://hub/resources/metrics/lustre_exporter"
-wget --no-check-certificate "http://hub/resources/metrics/gpfs_exporter"
