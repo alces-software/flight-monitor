df5230c8b214dcb023ff21ad90f2e9181b37aa11
twshaw646 <112694361+twshaw646@users.noreply.github.com>
2022-11-04 11:34:08 +0000

    Recreated curlAll.sh

---
+++ prometheus/vmsetup/curlAll.sh
@@ -0,0 +1,5 @@
+#Grab bins from hub
+curl "http://hub/resources/metrics/lustre_exporter"
+curl "http://hub/resources/metrics/slurm_exporter"
+curl "http://hub/resources/metrics/lustre_exporter"
+curl "http://hub/resources/metrics/gpfs_exporter"
