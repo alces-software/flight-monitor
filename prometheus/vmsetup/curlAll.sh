9f0f3c605bd806d77172841d53f552ff2997f5c8
twshaw646 <112694361+twshaw646@users.noreply.github.com>
2022-11-04 11:59:24 +0000

    Update and rename curlAll.sh to wgetAll.sh

--- prometheus/vmsetup/curlAll.sh
+++
@@ -1,5 +0,0 @@
-#Grab bins from hub
-curl "http://hub/resources/metrics/lustre_exporter"
-curl "http://hub/resources/metrics/slurm_exporter"
-curl "http://hub/resources/metrics/lustre_exporter"
-curl "http://hub/resources/metrics/gpfs_exporter"
---
+++ prometheus/vmsetup/wgetAll.sh
@@ -0,0 +1,5 @@
+#Grab bins from hub
+wget --no-check-certificate "http://hub/resources/metrics/lustre_exporter"
+wget --no-check-certificate "http://hub/resources/metrics/slurm_exporter"
+wget --no-check-certificate "http://hub/resources/metrics/lustre_exporter"
+wget --no-check-certificate "http://hub/resources/metrics/gpfs_exporter"
